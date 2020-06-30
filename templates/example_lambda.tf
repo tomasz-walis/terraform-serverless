9fc9cbcc31aee4388d13c06649f61bdef9c35644
Tomasz Walis <tomasz.walis@ecs.co.uk>
2020-06-30 16:19:07 +0100

    change lambda version

--- templates/example_lambda.tf
+++ templates/example_lambda.tf
@@ -1,14 +1,17 @@
 provider "aws" {
   region = "us-east-1"
   profile = "aad"
- }
+}
+
+variable "app_version" {
+}
 
 resource "aws_lambda_function" "example" {
  function_name = "ServerlessExample"
 
  # The bucket name as created earlier with "aws s3api create-bucket"
  s3_bucket = "terraform-serverless-tomasz"
- s3_key    = "v1.0.0/example.zip"
+ s3_key    = "v${var.app_version}/example.zip"
 
  # "main" is the filename within the zip file (main.js) and "handler"
  # is the name of the property under which the handler function was
